b720840b5bafe025571df3b9142d641df7d5098e
vharatyk <haratyk2005@gmail.com>
2024-02-14 12:55:56 +0100

    4%

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 22:15:55 by vharatyk          #+#    #+#             */
-/*   Updated: 2024/02/10 17:26:24 by vharatyk         ###   ########.fr       */
+/*   Updated: 2024/02/12 15:03:32 by vharatyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,48 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <pthread.h>
+#include <pthread.h>
+#include <sys/time.h>
 
+typedef pthread_mutex_t t_mtx ;
 
 typedef struct s_data
 {
 int nb_philo;
-int time_deth;
-int time_eat;
-int time_slepp;
-int long deltat;
+long time_deth;
+long time_eat;
+long time_slepp;
+long deltat;
+long	round; // optionale
+t_fork *forks ;
+t_philo *philos ;
 }			t_data ;
+
+
+typedef struct s_fork
+{
+	t_mtx fork;
+	int	fork_id;
+} t_fork;
 
 
 typedef struct s_philo
 {
-	int index;
-	long int time eat;
-	long int sleep;
-	t_fork left;
-	t_fork right;
-	pthread_t thead;
+	int index; 
+
+	long int time_eat;
+	long int last_eat;
+
+	long int time_sleep;
+	t_fork *left;
+	t_fork *right;
+	pthread_t thead_id;
+	t_data *data ;
 
 
 } t_philo ;
 
 int ft_erreur(void);
-
 int ft_atoi(char *str);
 
 
--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: vharatyk <vharatyk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 22:15:32 by vharatyk          #+#    #+#             */
-/*   Updated: 2024/02/10 17:06:04 by vharatyk         ###   ########.fr       */
+/*   Updated: 2024/02/13 22:35:24 by vharatyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,47 +17,38 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-//  echo "\x1b[38;2;255;0;0;5mSal\x1b[38;2;0;255;0;7mu\e[0mt"
+// color :  echo "\x1b[38;2;255;0;0;5mSal\x1b[38;2;0;255;0;7mu\e[0mt"
 
-// static void	*routine(void *arg)
-// {
-// 	int i = 0 ;
-// 	int *num ;
 
-// 	num = (int *)arg;
-// 	while(i < 10000)
-// 	{
-// 			(*num)++;
-// 			i++;
-// 	}
-// 	return (NULL);
-// }
+int init(t_data *data)
+{
+	data->philos = malloc(sizeof(t_philo) * data->nb_philo);
+	data->forks = malloc(sizeof(t_fork)* data->nb_philo);
 
-// void example()
-// {
-// 	pthread_t	t1;
-// 	pthread_t	t2;
-// 	int *num ;
 
-// 	num = malloc(sizeof(int));
+}
 
-// 	*num = 0 ;
-// 	if(pthread_create(&t1, NULL ,routine, num))
-// 		exit(1);
-// 	if(pthread_create(&t2, NULL ,routine, num))
-// 		exit(1);
-
-// 	if(pthread_join(t1, NULL))
-// 		exit(1);
-// 	if(pthread_join(t2, NULL))
-// 		exit(1);
-// 	printf("NUM: %d", *num);
-// 	}
-
-int cretate_philo(t_data data)
+int parsing(t_data *data , char **argv , int argc)
 {
 
-	while()
+int j = 1 ;
+	while(argc != j)
+	{
+		if(is_char_neg(argv[j]))
+			return(ft_erreur());
+		j++;
+	}
+	data->nb_philo = ft_atoi(argv[1]) ;
+	data->time_deth = ft_atoi(argv[2]) ;
+	data->time_eat = ft_atoi(argv[3]) ;
+	data->time_slepp = ft_atoi(argv[4]);
+	data->deltat = 0 ;
+	if(argc == 6)
+	{
+		data->round = ft_atoi(argv[5]);
+	}
+
+	//data->deltat = get_time(data->nb_philo);
 
 
 }
@@ -102,28 +93,17 @@ int main (int argc , char **argv)
 {
 	t_data data ;
 
-if(argc == 4 || argc == 5)
+if(argc == 5 || argc == 6)
 {
-	int j = 1 ;
-	while(argc != j)
-	{
-		if(is_char_neg(argv[j]))
-			return(ft_erreur());
-		j++;
-	}
-	data.nb_philo = ft_atoi(argv[0]) ;
-	data.time_deth = ft_atoi(argv[1]) ;
-	data.time_eat = ft_atoi(argv[2]) ;
-	data.time_slepp = ft_atoi(argv[3]);
-	data.deltat = 0 ;
+	parsing( &data , argv , argc); // parsing
+
 	data.deltat = get_time(data.deltat);
-
-
-	printf("%ld ",get_time(data.deltat));
+	printf("%ld ",get_time(data.deltat)); 
 	sleep(1);
 	printf("%ld ",get_time(data.deltat));
 
 }
+	error_exit();
 
 	return(0);
 }
